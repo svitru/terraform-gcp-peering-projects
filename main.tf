7d6d1bdc5eb81f699f7d50e37403167a1fd239e3
С.В <sv@sv-it.ru>
2020-03-05 00:11:01 +0200

    add cloud sql and private network

--- main.tf
+++ main.tf
@@ -133,3 +133,49 @@ resource "google_compute_instance" "vm_instance_b" {
   }
 }
 
+resource "google_compute_network" "private_network" {
+#  provider = google-beta
+  project = var.project_a
+
+  name = "private-network"
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+#  provider = google-beta
+  project = var.project_a
+
+  name          = "private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.private_network.self_link
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+#  provider = google-beta
+
+  network                 = google_compute_network.private_network.self_link
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "google_sql_database_instance" "instance" {
+#  provider = google-beta
+  project = var.project_a
+
+  name   = "private-instance-mysql"
+  region = "us-central1"
+
+  database_version = "MYSQL_5_7"
+
+  depends_on = [google_service_networking_connection.private_vpc_connection]
+
+  settings {
+    tier = "db-f1-micro"
+    ip_configuration {
+      ipv4_enabled    = false
+      private_network = google_compute_network.private_network.self_link
+    }
+  }
+}
+
